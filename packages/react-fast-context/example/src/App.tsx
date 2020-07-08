f46b9f655fea96900407eb65d46c5a533c41fc69
Adam Borowski <fl.borovsky@gmail.com>
2020-07-08 20:03:07 +0200

    chore: add react-fastt-context library skeleton

---
+++ packages/react-fast-context/example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-fast-context'
+import 'react-fast-context/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
