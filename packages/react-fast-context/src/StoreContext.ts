e60ef1989d3fbc78beb335bfe7112026cf73196f
Adam Borowski <fl.borovsky@gmail.com>
2020-07-13 09:15:44 +0200

    feat: added one export

--- packages/react-fast-context/src/StoreContext.ts
+++ packages/react-fast-context/src/StoreContext.ts
@@ -2,3 +2,5 @@ import { createContext } from 'react';
 import { Store } from './types';
 
 export const StoreContext = createContext<Store<unknown> | undefined>(undefined);
+
+export const StoreProvider = StoreContext.Provider;
